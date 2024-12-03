26703e8a2d37946fa32df188db9d08b23aca530d
avotaangi <alinaignatova67@gmail.com>
2024-12-04 01:43:31 +0300

    Fix: auth_views with allow any

--- InterviewHub/users/views/auth_views.py
+++ InterviewHub/users/views/auth_views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,6 +11,7 @@ from ..serializers import RegisterSerializer
 
 
 class AuthViewSet(ViewSet):
+    permission_classes = [AllowAny]
     @action(detail=False, methods=['post'])
     @swagger_auto_schema(
         operation_summary="Регистрация пользователя",
