f99306d9a400af7e3bb1c6e01cfd0d6f2323490f
avotaangi <alinaignatova67@gmail.com>
2024-12-08 21:11:46 +0300

    Fix: verbose_name

--- InterviewHub/selections/apps.py
+++ InterviewHub/selections/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class SelectionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'selections'
-    verbose_name = "Отборы кандидатов"
+    verbose_name = "Отбор кандидатов"
