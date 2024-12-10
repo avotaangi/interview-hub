ea6f579f7b133714585453273d06d7504d9ae855
avotaangi <alinaignatova67@gmail.com>
2024-12-10 21:48:51 +0300

    Add: possibility for tasks add answer in admin panel

--- InterviewHub/tasks/admin.py
+++ InterviewHub/tasks/admin.py
@@ -1,14 +1,58 @@
 from django.contrib import admin
 from .models import TaskItem, OpenQuestion, MultipleChoiceQuestion, CodeQuestion
+from django.contrib.admin import SimpleListFilter
+
+
+class TaskTypeFilter(SimpleListFilter):
+    title = "Тип задания"
+    parameter_name = "task_type"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("open_question", "С открытым ответом"),
+            ("multiple_choice", "С выбором ответа"),
+            ("code_question", "С написанием кода"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "open_question":
+            return queryset.filter(openquestion__isnull=False)
+        elif self.value() == "multiple_choice":
+            return queryset.filter(multiplechoicequestion__isnull=False)
+        elif self.value() == "code_question":
+            return queryset.filter(codequestion__isnull=False)
+        return queryset
+
+class OpenQuestionInline(admin.StackedInline):
+    model = OpenQuestion
+    extra = 0
+    verbose_name = "Задание с открытым ответом"
+    verbose_name_plural = "Задания с открытым ответом"
+
+
+class MultipleChoiceQuestionInline(admin.StackedInline):
+    model = MultipleChoiceQuestion
+    extra = 0
+    verbose_name = "Задание с выбором ответа"
+    verbose_name_plural = "Задания с выбором ответа"
+
+
+class CodeQuestionInline(admin.StackedInline):
+    model = CodeQuestion
+    extra = 0
+    verbose_name = "Задание с написанием кода"
+    verbose_name_plural = "Задания с написанием кода"
 
 
 @admin.register(TaskItem)
 class TaskItemAdmin(admin.ModelAdmin):
     list_display = ("title", "complexity")
     search_fields = ("title",)
-    list_filter = ("complexity",)  # Фильтр по сложности
+    list_filter = ("complexity",TaskTypeFilter)  # Фильтр по сложности
     list_display_links = ("title",)  # Сделаем "title" ссылкой
 
+    # Добавление inline
+    inlines = [OpenQuestionInline, MultipleChoiceQuestionInline, CodeQuestionInline]
 
 @admin.register(OpenQuestion)
 class OpenQuestionAdmin(admin.ModelAdmin):
