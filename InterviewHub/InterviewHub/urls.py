fca06222db15c9ac5baa24a5bb0768e0743d26c4
avotaangi <alinaignatova67@gmail.com>
2024-12-23 02:00:49 +0300

    Update: urls.py

--- InterviewHub/InterviewHub/urls.py
+++ InterviewHub/InterviewHub/urls.py
@@ -61,5 +61,5 @@ urlpatterns = [
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
-    path("accounts/", include("allauth.urls")),
+    # path("accounts/", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
