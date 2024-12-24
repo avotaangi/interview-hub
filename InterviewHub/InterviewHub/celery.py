cee55ff9880dc2173f30d19f13fb9ecf5fe31309
avotaangi <alinaignatova67@gmail.com>
2024-12-24 04:50:57 +0300

    Fix: celery

--- InterviewHub/InterviewHub/celery.py
+++ InterviewHub/InterviewHub/celery.py
@@ -14,19 +14,17 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Автоматически обнаруживаем задачи из приложений
 app.autodiscover_tasks()
 
+
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+
 
 app.conf.beat_schedule = {
     'send_reminders_every_hour': {
         'task': 'interviews.tasks.send_interview_reminder',
         'schedule': crontab(minute='*'),  # Каждую минуту
     },
-}
-
-app.conf.beat_schedule = {
-    # Архивация "Отклоненных" CompanySelection раз в неделю
     'archive_rejected_selections': {
         'task': 'selections.tasks.archive_rejected_company_selections',
         'schedule': crontab(day_of_week='sunday', hour=0, minute=0),  # Каждый воскресенье в 00:00
