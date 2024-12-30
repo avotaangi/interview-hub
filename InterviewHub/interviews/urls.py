8184989bdf96c3b667e8ba5426b925205663c8eb
avotaangi <alinaignatova67@gmail.com>
2024-12-30 03:51:39 +0300

    Add: views and templates

--- InterviewHub/interviews/urls.py
+++ InterviewHub/interviews/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.interview_viewset import InterviewViewSet
 from .views.interview_task_viewset import InterviewTaskItemViewSet
-from .views.template_views import interview_list_view
+from .views.template_views import interview_list_view, interview_create_view, interview_edit_view, interview_delete_view
 
 router = DefaultRouter()
 router.register(r"interviews", InterviewViewSet, basename="interview")
@@ -10,5 +10,8 @@ router.register(r"interview-tasks", InterviewTaskItemViewSet, basename="intervie
 
 urlpatterns = router.urls
 urlpatterns += [
-    path('interviews/list', interview_list_view, name='interview_list'),
+    path('interviews/show/list', interview_list_view, name='interview_list'),
+    path('interviews/new', interview_create_view, name='interview_create'),
+    path('interviews/<int:pk>/edit', interview_edit_view, name='interview_edit'),
+    path('interviews/<int:pk>/delete', interview_delete_view, name='interview_delete'),
 ]
--- InterviewHub/interviews/views/template_views.py
+++ InterviewHub/interviews/views/template_views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from ..models import Interview
+from ..forms import InterviewForm
 
 
 def interview_list_view(request):
     interviews = Interview.objects.select_related('selection__resume__candidate__user').all()
     return render(request, 'interviews/interview_list.html', {'interviews': interviews})
+
+def interview_create_view(request):
+    if request.method == 'POST':
+        form = InterviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('interview_list')  # Перенаправление на список интервью
+    else:
+        form = InterviewForm()
+    return render(request, 'interviews/interview_form.html', {'form': form})
+
+def interview_edit_view(request, pk):
+    interview = get_object_or_404(Interview, pk=pk)
+    if request.method == 'POST':
+        form = InterviewForm(request.POST, request.FILES, instance=interview)
+        if form.is_valid():
+            form.save()
+            return redirect('interview_list')
+    else:
+        form = InterviewForm(instance=interview)
+    return render(request, 'interviews/interview_form.html', {'form': form})
+
+def interview_delete_view(request, pk):
+    interview = get_object_or_404(Interview, pk=pk)
+    if request.method == 'POST':
+        interview.delete()
+        return redirect('interview_list')
+    return render(request, 'interviews/interview_confirm_delete.html', {'interview': interview})
+
