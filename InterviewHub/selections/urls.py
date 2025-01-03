37a636d8bc39fea95d3490e345195ccf0eae1965
avotaangi <alinaignatova67@gmail.com>
2025-01-04 01:00:49 +0300

    Add: {{ field.errors }} {{ field.label_tag }} {{ field }}

--- InterviewHub/selections/urls.py
+++ InterviewHub/selections/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.company_selection_views import CompanySelectionViewSet
-from .views.template_company_selection import company_selection_create
+from .views.template_company_selection import company_selection_create, company_selection_view, \
+    company_selection_success
 
 # Создаем роутер для ViewSet
 router = DefaultRouter()
@@ -12,5 +13,8 @@ router.register(
 urlpatterns = router.urls
 
 urlpatterns += [
-    path('selections/create', company_selection_create, name='company_selection_create')
+    path('selections/create', company_selection_create, name='company_selection_create'),
+    path('selections/', company_selection_view, name='company_selection'),
+    path('selections/<int:pk>/', company_selection_view, name='edit_company_selection'),
+    path('selections/success/', company_selection_success, name='company_selection_success'),
 ]
--- InterviewHub/selections/views/template_company_selection.py
+++ InterviewHub/selections/views/template_company_selection.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, redirect
 from ..forms.company_selection_form import CompanySelectionForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+
+from ..models import CompanySelection
+
 
 def company_selection_create(request):
     if request.method == 'POST':
@@ -12,3 +16,35 @@ def company_selection_create(request):
     else:
         form = CompanySelectionForm()
         return render(request, 'selections/company_selection_form.html', {'form': form})
+
+
+def company_selection_view(request, pk=None):
+    """
+    Представление для создания или редактирования отбора компании.
+    """
+    if pk:
+        # Если передан pk, получаем существующую запись
+        instance = get_object_or_404(CompanySelection, pk=pk)
+    else:
+        # Иначе создаем новую запись
+        instance = None
+
+    if request.method == 'POST':
+        # Если отправлен POST-запрос, заполняем форму данными
+        form = CompanySelectionForm(request.POST, instance=instance)
+        if form.is_valid():
+            form.save()  # Сохраняем данные формы
+            return redirect('success/')  # Перенаправление после сохранения
+    else:
+        # Если GET-запрос, показываем пустую или заполненную форму
+        form = CompanySelectionForm(instance=instance)
+
+    # Рендеринг шаблона
+    return render(request, 'selections/company_selection_detailed.html', {'form': form})
+
+
+def company_selection_success(request):
+    """
+    Представление для отображения успешного результата после сохранения формы.
+    """
+    return HttpResponse("<h1>Форма успешно сохранена!</h1>")
