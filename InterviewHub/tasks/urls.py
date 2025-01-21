b621cc0339e615a5ae1c7d7fda663ce3caa70c17
avotaangi <alinaignatova67@gmail.com>
2025-01-21 05:56:41 +0300

    Add: add search in tasks list

--- InterviewHub/tasks/urls.py
+++ InterviewHub/tasks/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('tasks/open/', tasks_with_open_questions, name='tasks_with_open_questions'),
     path('tasks/code/', tasks_with_code_questions, name='tasks_with_code_questions'),
     path('tasks/<int:task_id>/', task_detail, name='task_detail'),
+
 ]
