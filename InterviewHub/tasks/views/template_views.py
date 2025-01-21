27b057af7bc319a9584c72f8525c85b5a5021406
avotaangi <alinaignatova67@gmail.com>
2025-01-21 05:56:31 +0300

    Add: add search in tasks list

--- InterviewHub/tasks/views/template_views.py
+++ InterviewHub/tasks/views/template_views.py
@@ -2,17 +2,40 @@ from django.shortcuts import render, get_object_or_404
 from ..models import TaskItem
 
 def tasks_with_choices(request):
-    tasks = set(TaskItem.objects.filter(multiplechoicequestion__isnull=False))
-    return render(request, 'tasks/tasks_list.html', {'tasks': tasks, 'title': 'Задания с вариантами ответов'})
+    query = request.GET.get('query', '').strip()
+    tasks = TaskItem.objects.filter(multiplechoicequestion__isnull=False)
+
+    if query:
+        tasks = tasks.filter(title__icontains=query)
+
+    return render(request, 'tasks/tasks_list.html', {
+        'title': 'Задания с вариантами ответов',
+        'tasks': set(tasks),
+    })
 
 def tasks_with_open_questions(request):
-    tasks = set(TaskItem.objects.filter(openquestion__isnull=False))
-    return render(request, 'tasks/tasks_list.html', {'tasks': tasks, 'title': 'Задания с открытыми вопросами'})
+    query = request.GET.get('query', '').strip()
+    tasks = TaskItem.objects.filter(openquestion__isnull=False)
+
+    if query:
+        tasks = tasks.filter(title__icontains=query)
+
+    return render(request, 'tasks/tasks_list.html', {
+        'title': 'Задания с открытыми вопросами',
+        'tasks': set(tasks),
+    })
 
 def tasks_with_code_questions(request):
-    tasks = set(TaskItem.objects.filter(codequestion__isnull=False))
-    return render(request, 'tasks/tasks_list.html', {'tasks': tasks, 'title': 'Задания с написанием кода'})
+    query = request.GET.get('query', '').strip()
+    tasks = TaskItem.objects.filter(codequestion__isnull=False)
 
+    if query:
+        tasks = tasks.filter(title__icontains=query)
+
+    return render(request, 'tasks/tasks_list.html', {
+        'title': 'Задания с написанием кода',
+        'tasks': set(tasks),
+    })
 def task_detail(request, task_id):
     task = get_object_or_404(TaskItem, id=task_id)
     return render(request, 'tasks/task_detail.html', {'task': task})
